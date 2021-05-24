0ff03112113825d61709a26351b57f2c67cc48ab
RavenR3X <78075608+RavenR3X@users.noreply.github.com>
2021-05-24 11:37:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function nextPage()
+{
+    window.location.href = "instruct.html";
+}
+
+function takeTest()
+{
+    window.location.href = "test.html";
+}
+
+function takeToAnswer()
+{
+    window.location.href = "answer.html"
+}
